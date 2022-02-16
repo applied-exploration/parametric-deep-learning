63c82fffccacdb01fde8a935a30dab1a9a9420b3
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-02-16 19:27:25 +0100

    feat(Embedding): super basic embedding of commands

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,10 +1,16 @@
-import csv 
 import pandas as pd
 import math
 import random
 import matplotlib.pyplot as plt
-    
+from dataclasses import dataclass
 
+
+@dataclass
+class DataConfig:
+    canvas_size:int
+    max_radius:int
+    num_sample_points:int
+    num_circles:int
             
 def random_circle_point(radius:float)->tuple[float, float]:
     angle = random.random()*math.pi*2
@@ -17,20 +23,20 @@ def translation(x:float, y:float, random_translation_x:float, random_translation
     translated_y = y+random_translation_y
     return translated_x, translated_y
 
-def generate_dataset(canvas_size:int, max_radius:int, num_sample_points:int, num_circles:int, display_plot:bool=False):
+def generate_dataset(config: DataConfig, display_plot:bool=False):
     column_names = ["features", "label"]
 
     df = pd.DataFrame(columns=column_names)
     
-    for i in range(num_circles):
+    for i in range(config.num_circles):
         random_samples = []
         plain_samples = []
         
-        random_circle_radius = min(max_radius, random.random()*max_radius)
-        random_translation_x = random.uniform(-1,1) * (canvas_size/2-max_radius)
-        random_translation_y = random.uniform(-1,1) * (canvas_size/2-max_radius)
+        random_circle_radius = min(config.max_radius, random.random()*config.max_radius)
+        random_translation_x = random.uniform(-1,1) * (config.canvas_size/2-config.max_radius)
+        random_translation_y = random.uniform(-1,1) * (config.canvas_size/2-config.max_radius)
         
-        for j in range(num_sample_points):
+        for j in range(config.num_sample_points):
             x, y = random_circle_point(random_circle_radius)
             x, y = translation(x, y, random_translation_x, random_translation_y)
             random_samples.extend([str(x) + " " + str(y) + '\n'])
@@ -47,17 +53,14 @@ def generate_dataset(canvas_size:int, max_radius:int, num_sample_points:int, num
             zipped = list(zip(*plain_samples))
             plt.gca().set_aspect('equal')
             plt.scatter(zipped[0], zipped[1])
-            plt.xlim(-canvas_size/2,canvas_size)
-            plt.ylim(-canvas_size/2,canvas_size)
+            plt.xlim(-config.canvas_size/2,config.canvas_size)
+            plt.ylim(-config.canvas_size/2,config.canvas_size)
             plt.show()
         
     df.to_csv('data/dataset.csv', index=True)
 
 if __name__ == '__main__':
-    canvas_size:int = 100
-    max_radius:int = 20
-    num_sample_points:int = 100
-    num_circles:int = 500
+    dataconfig = DataConfig(canvas_size=100, max_radius=20, num_sample_points=100, num_circles=500)
 
-    generate_dataset(canvas_size, max_radius, num_sample_points, num_circles, display_plot=False)   
+    generate_dataset(dataconfig, display_plot=False)   
         
---
+++ utils/quantize.py
@@ -0,0 +1,4 @@
+import torch
+
+def quantize(input: torch.Tensor, n=256) -> torch.Tensor:
+    return input.round().clip(min=0, max=n-1)
