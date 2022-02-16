d89dd3bc7f2e771b0550aafd313bfe83c425ddeb
Daniel Szemerey <szemereydaniel@gmail.com>
2022-02-16 15:21:01 +0100

    feat: Created Dataset generator and generated a 500 piece dataset with 100 sampling.

---
+++ generate_dataset.py
@@ -0,0 +1,69 @@
+import csv 
+import pandas as pd
+import math
+import random
+import matplotlib.pyplot as plt
+    
+
+
+def write_csv(path, file):
+    with open(path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(file)
+            
+def random_circle_point(radius):
+    angle = random.random()*math.pi*2
+    x = math.cos(angle)*radius
+    y = math.sin(angle)*radius
+    return x, y
+
+def translation(x, y, random_translation_x, random_translation_y):
+    translated_x = x+random_translation_x
+    translated_y = y+random_translation_y
+    return translated_x, translated_y
+
+def generate_dataset(canvas_size, max_radius, num_sample_points, num_circles, display_plot=False):
+    column_names = ["features", "label"]
+
+    df = pd.DataFrame(columns=column_names)
+    
+    for i in range(num_circles):
+        random_samples = []
+        plain_samples = []
+        
+        random_circle_radius = min(max_radius, random.random()*max_radius)
+        random_translation_x = random.uniform(-1,1) * (canvas_size/2-max_radius)
+        random_translation_y = random.uniform(-1,1) * (canvas_size/2-max_radius)
+        
+        for j in range(num_sample_points):
+            x, y = random_circle_point(random_circle_radius)
+            x, y = translation(x, y, random_translation_x, random_translation_y)
+            random_samples.extend([str(x) + " " + str(y) + '\n'])
+            
+            if display_plot: plain_samples.extend([(x, y)])
+        
+        features_collapsed = "".join(random_samples)
+        labels_collapsed = "".join(["CIRCLE {}\n".format(random_circle_radius), "TRANSLATION {} {}".format(random_translation_x, random_translation_y)])
+        one_data_point = pd.Series(dict(zip(column_names, [features_collapsed, labels_collapsed])))
+        df = df.append(one_data_point, ignore_index=True)
+        
+        ## Plot for reality check
+        if display_plot:
+            zipped = list(zip(*plain_samples))
+            fig, ax = plt.subplots()
+            plt.gca().set_aspect('equal')
+            plt.scatter(zipped[0], zipped[1])
+            plt.xlim(-canvas_size/2,canvas_size)
+            plt.ylim(-canvas_size/2,canvas_size)
+            plt.show()
+        
+    df.to_csv('data/dataset.csv', index=True)
+
+if __name__ == '__main__':
+    canvas_size = 100
+    max_radius = 20
+    num_sample_points = 100
+    num_circles = 500
+
+    generate_dataset(canvas_size, max_radius, num_sample_points, num_circles, display_plot=False)   
+        
