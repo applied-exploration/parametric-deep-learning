b758cbf4d5996b37a99819f399641825e2e60636
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-02-16 19:34:16 +0100

    feat(Tests): wire up pytest

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,10 +1,5 @@
-import unittest
-
-import sys
-sys.path.append('.')
-
 from utils.loss import calculate_label_loss, calculate_symbolic_loss
-from parse import parse_program, parse_points
+from parse import parse_program
 
 example_program_good = "CIRCLE 1.3790708713968214\nTRANSLATION -24.86442070352715 8.947517177527358"
 example_program_bad = "CIRCLE 0.3790708713968214\nTRANSLATION -22.86442070352715 7.947517177527358"
@@ -13,16 +8,13 @@ parsed_program = parse_program(example_program_good)
 estimated_program_values = [parsed_program[0].r, parsed_program[1].x, parsed_program[1].y]
 actual_values = [1.3790708713968214, -24.86442070352715, 8.947517177527358]
 
-class TestLossMethods(unittest.TestCase):
-    def test_symbolic_loss(self) -> None:
-        print("Testing loss based on distance between circle and points")
-        symbolic_loss = calculate_symbolic_loss(example_program_good, example_points)
-        assert -0.0001 <= symbolic_loss <= 0.0001, "Symbolic loss should be 0.0, it was {}".format(symbolic_loss)
+def test_symbolic_loss() -> None:
+    print("Testing loss based on distance between circle and points")
+    symbolic_loss = calculate_symbolic_loss(example_program_good, example_points)
+    assert -0.0001 <= symbolic_loss <= 0.0001, "Symbolic loss should be 0.0, it was {}".format(symbolic_loss)
+
+def test_label_loss() -> None:
+    print("Testing loss based on difference between estimated and actual values")
+    label_loss = calculate_label_loss(estimated_program_values, actual_values)
+    assert -0.0001 <= label_loss <= 0.0001, "Label loss should be 0.0, it was {}".format(label_loss)
     
-    def test_label_loss(self) -> None:
-        print("Testing loss based on difference between estimated and actual values")
-        label_loss = calculate_label_loss(estimated_program_values, actual_values)
-        assert -0.0001 <= label_loss <= 0.0001, "Label loss should be 0.0, it was {}".format(label_loss)
-        
-if __name__ == '__main__':
-    unittest.main()
