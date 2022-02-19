faa9dd66e857dde12c83935ae5ca7ebdcdd1e5da
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-02-19 18:53:55 +0100

    fix(Linter)

--- data/data_loader.py
+++ data/data_loader.py
@@ -6,4 +6,9 @@ def load_data() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     df = pd.read_csv("data/dataset.csv")
     train = df.sample(frac=0.8, random_state=200)
     test = df.drop(train.index)
-    return train["features"].to_numpy(), train["label"].to_numpy(), test["features"].to_numpy(), test["label"].to_numpy()
+    return (
+        train["features"].to_numpy(),
+        train["label"].to_numpy(),
+        test["features"].to_numpy(),
+        test["label"].to_numpy(),
+    )
