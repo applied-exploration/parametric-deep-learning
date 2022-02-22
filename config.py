c6f93711047153e9dc1a6f87afa75705531a611f
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-02-22 14:09:29 +0100

    feature(Pipeline): introduce ProgramSynthesisTask encapsulating all the information about a specific PS ML problem

--- config.py
+++ config.py
@@ -1,4 +1,21 @@
+from dataclasses import dataclass
+from typing import Callable
 from data.types import DataConfig
+from models.types import Model
+
+
+@dataclass(frozen=True)
+class ProgramSynthesisTask:
+    data_loader: Callable
+    instructions_map: dict
+    parse_input: Callable
+    parse_program: Callable
+    embed_input: Callable
+    embed_program: Callable
+    embedding_to_program: Callable
+    scorer: Callable
+    model: Model
+
 
 dataconfig = DataConfig(
     canvas_size=100,
