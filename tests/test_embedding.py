bb85f3b72b2c26c8d040c5eb6b639de5a879a67d
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-03 16:31:53 +0100

    fix(Tests)

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -1,14 +1,17 @@
 import numpy as np
-from config import dataconfig
+from config import dataconfig_3
 from data.types import Circle, Translate, Constraint
 from embedding import embed_instructions, from_embeddings_to_instructions
 
 instructions = [
     Circle(5.0, 51.0, 14.0, 0.0),
-    Translate(10.0, 15.0, index=3),
-    Translate(15.0, 25.0, index=4),
+    Circle(6.0, 51.0, 14.0, 0.0),
+    Translate(10.0, 15.0, index=0),
+    Translate(15.0, 25.0, index=0),
     Constraint(x=25.0, y=90.0, indicies=(0, 1)),
 ]
+
+dataconfig = dataconfig_3
 
 
 def test_embeddings():
