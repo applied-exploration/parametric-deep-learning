82d0864702167820440a645ca22c034e1027e7fb
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-04 17:50:47 +0100

    feat(Training): added validation dataset / evaluation for each epoch

--- models/neural.py
+++ models/neural.py
@@ -11,10 +11,22 @@ class LightningNeuralNetModel(Model):
         self.model = model
         self.trainer = pl.Trainer(max_epochs=max_epochs)
 
-    def fit(self, X: list[torch.Tensor], y: list[torch.Tensor]) -> None:
-        dataloader = get_dataloader(X, y)
-        self.model.initialize_network(X[0].shape, y[0].shape[0])
-        self.trainer.fit(self.model, dataloader)
+    def fit(
+        self,
+        X_train: list[torch.Tensor],
+        y_train: list[torch.Tensor],
+        X_val: list[torch.Tensor],
+        y_val: list[torch.Tensor],
+    ) -> None:
+        train_dataloader = get_dataloader(X_train, y_train)
+        val_dataloader = get_dataloader(X_val, y_val)
+
+        self.model.initialize_network(X_train[0].shape, y_train[0].shape[0])
+        self.trainer.fit(
+            self.model,
+            train_dataloaders=train_dataloader,
+            val_dataloaders=val_dataloader,
+        )
 
     def predict(self, X: list[torch.Tensor]) -> np.ndarray:
         return self.model(X)
